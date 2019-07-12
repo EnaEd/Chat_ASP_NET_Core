4276fc3d419b992e551e8c9abfcadd205c4d7937
ena.ed <djaziz12393@gmail.com>
2019-07-12 12:06:01 +0300

    add crud repository

---
+++ Chat.DAL/Interfaces/ICRUDRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.DAL.Interfaces
+{
+    public interface ICRUDRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Create(T item);
+        void Delete(int id);
+        T GetById(int id);
+    }
+}
---
+++ Chat.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using Chat.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.DAL.Repositories
+{
+    public class BaseRepository<T> : ICRUDRepository<T> where T : class
+    {
+        public BaseRepository()
+        {
+
+        }
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return 
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
